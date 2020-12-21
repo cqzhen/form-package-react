d043e7df0890e0d878ce70ecc1be41852ddf75b7
cqzhen <1071535051@qq.com>
2020-12-21 12:18:11 +0800

    modify to public

--- examples/src/fields.js
+++ examples/src/fields.js
@@ -1,11 +1,31 @@
 const fields = [
-	{id: 'userName', label: '姓名', type: 'input'},
-	{id: 'company', label: '公司', type: 'input' },
-	{id: 'sex', label: '性别', type: 'radio', options: [{label: '男', value: 1}, {label: '女', value: 2}] },
-	{id: 'img1', label: '个人相片', type: 'inputFile'},
-	{id: 'provice', label: '省份', type: 'select', options: [{label: '北京', value: 1}, {label: '上海', value: 2}, {label: '广州', value: 3},{label: '深圳', value: 4},{label: '武汉', value: 5},{label: '郑州', value: 6} ] },
-	{id: 'introduce', label: '简介', type: 'textarea'},
-	{id: 'img2', label: '个人相片', type: 'inputFile'},
+	{
+    id: 'userName',
+    label: '姓名',
+    type: 'input',
+    require: true,
+    remindText: '',
+    nullText: '内容不为空',
+    maxLength: 10,
+  },
+	{
+    id: 'phone',
+    label: '电话',
+    type: 'input',
+    require: false,
+    remindText: '',
+    regex: /^((0\d{2,3}-\d{7,8})|(1[3456789]\d{9}))$/,
+    nullText: '内容不为空(其实这个没有用，因为 require false)',
+    regexText: '请填写正确的电话号码',
+    maxLength: 12,
+  },
+	{id: 'birthDay', label: '出生日期', type: 'date', require: true, remindText: '', nullText: '内容不为空' },
+	{id: 'company', label: '公司', type: 'input', require: false, remindText: '', nullText: '内容不为空', maxLength: 10 },
+	{id: 'sex', label: '性别', type: 'radio', options: [{label: '男', value: 1}, {label: '女', value: 2}], require: true, remindText: '', nullText: '内容不为空', },
+	{id: 'img1', label: '个人相片', type: 'inputFile', require: true, remindText: '', nullText: '内容不为空', },
+	{id: 'provice', label: '省份', type: 'select', options: [{label: '北京', value: 1}, {label: '上海', value: 2}, {label: '广州', value: 3},{label: '深圳', value: 4},{label: '武汉', value: 5},{label: '郑州', value: 6} ], require: true, remindText: '', nullText: '内容不为空', },
+	{id: 'introduce', label: '简介', type: 'textarea', require: true, remindText: '', nullText: '内容不为空', maxLength: 100 },
+	{id: 'img2', label: '个人相片', type: 'inputFile', require: true, remindText: '', nullText: '内容不为空', },
 ];
 
 export default fields;
--- examples/src/form.js
+++ examples/src/form.js
@@ -1,25 +1,23 @@
 import React from 'react';
+import Form from 'form-package-react';
 import data from './fields';
-import Form from './../../src/index';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.submit = this.submit.bind(this)
-  }
+	constructor(props) {
+		super(props);
+		this.submit = this.submit.bind(this);
+	}
 
-  render() {
-    return (
-			<div className="App" style={sectionCss}>
-    	  <header className="App-header">
-					<Form data={data || []} submit={this.submit.bind(this)}></Form>
-    	  </header>
-    	</div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<Form data={data || []} submit={this.submit} />
+			</div>
+		)
+	}
 
-	submit(data) {
-		console.log('text:', '我是例子。');
+	submit(data, e) {
+		// the data for submit form data
 		console.log('data:', data);
 	}
 
